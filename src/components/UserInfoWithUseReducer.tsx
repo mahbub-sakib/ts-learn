8aa70ea6db786f0f445c2e1e52e3a307cff0fcf1
mahbub-sakib <mahbub.sakib418@gmail.com>
2025-02-27 18:02:28 +0600

    till m1 v5 done

--- src/components/UserInfoWithUseReducer.tsx
+++ src/components/UserInfoWithUseReducer.tsx
@@ -1,31 +1,47 @@
-import React, { useReducer } from 'react';
+import React, { ChangeEvent, useReducer } from 'react';
+
+type Taction = {
+    type: string;
+    payload: string;
+}
 
 const initialState = {
     name: '',
     age: '',
-    hobbies: [],
+    hobbies: [] as string[],
 
 };
 
-const reducer = (currentState, action) => {
+const reducer = (currentState: typeof initialState, action: Taction) => {
     switch (action.type) {
         case 'addName':
-            return { ...currentState, name: 'Sakib' };
+            return { ...currentState, name: action.payload };
+        case 'addAge':
+            return { ...currentState, age: action.payload };
+        case 'addHobbies':
+            return { ...currentState, hobbies: [...currentState.hobbies, action.payload] };
         default:
-            break;
+            return currentState;
     }
 };
+
+
 
 const UserInfoWithUseReducer = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     console.log(state);
+
+    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(state);
+    }
     return (
-        <form >
-            <input onChange={() => dispatch({ type: "addName" })}
+        <form onSubmit={handleSubmit}>
+            <input onChange={(e) => dispatch({ type: "addName", payload: e.target.value })}
                 className='border border-purple-300 m-10' type='text' name='name' id='name' placeholder='name'></input>
-            <input
+            <input onChange={(e) => dispatch({ type: "addAge", payload: e.target.value })}
                 className='' type='number' name='age' id='age' placeholder='age'></input>
-            <input
+            <input onBlur={(e) => dispatch({ type: "addHobbies", payload: e.target.value })}
                 className='' type='text' name='hobbies' id='hobbies' placeholder='hobby'></input>
             <button type="submit" >Submit</button>
         </form>
